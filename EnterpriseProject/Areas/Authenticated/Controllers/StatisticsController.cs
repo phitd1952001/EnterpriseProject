f33b2b6846080c408383ba6d658fbfd977c25d3d
Dang Khoi <88148432+dankhoi@users.noreply.github.com>
2023-03-23 22:04:23 +0700

    Done statistic

---
+++ EnterpriseProject/Areas/Authenticated/Controllers/StatisticsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EnterpriseProject.Data;
+using EnterpriseProject.Utility;
+using EnterpriseProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnterpriseProject.Areas.Authenticated.Controllers
+{
+    [Area(SD.Authenticated)]
+    public class StatisticsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StatisticsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET
+        public IActionResult Index()
+        {
+            var topics = _db.Topics.ToList();
+            var statistics = new List<Statistic>();
+            foreach (var topic in topics)
+            {
+                var numberOfIdeas = _db.Ideas.Count(_ => _.TopicId == topic.Id);
+                var statistic = new Statistic()
+                {
+                    TopicName = topic.Name,
+                    NumberOfIdeas = numberOfIdeas
+                };
+                statistics.Add(statistic);
+            }
+            return View(statistics);
+        }
+    }
+}
---
+++ EnterpriseProject/ViewModel/Statistic.cs
@@ -0,0 +1,8 @@
+﻿namespace EnterpriseProject.ViewModels
+{
+    public class Statistic
+    {
+        public string TopicName { get; set; }
+        public int NumberOfIdeas { get; set; }
+    }
+}
